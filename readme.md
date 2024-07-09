6adfd3f86fcd975d525b25f8bd8b1484e16a10ba
suhao07 <3210102806@zju.edu.cn>
2024-07-09 16:18:43 +0800

    初始代码

---
+++ readme.md
@@ -0,0 +1,3 @@
+turtlebot4_discovery 是连接到机器人树莓派相关文件（对应课程文件第10页“4. 上位机终端配置discovery server”）
+nav2_straightline_planner 是直线规划器（需要自行修改其中的函数，对应课程文件第15页“7.2 自定义路径规划器”）
+
---
+++ turtlebot4_discovery/setup.bash
@@ -0,0 +1,6 @@
+source /opt/ros/humble/setup.bash
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export FASTRTPS_DEFAULT_PROFILES_FILE=/etc/turtlebot4_discovery/fastdds_discovery_super_client.xml
+export ROS_DISCOVERY_SERVER=10.42.0.1:11811
+export ROS_DOMAIN_ID=0
+export RMW_FASTRTPS_USE_QOS_FROM_XML=1
